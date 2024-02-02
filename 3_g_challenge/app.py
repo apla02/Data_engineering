8b83d684b43eb6be7f9ef6f8346a8c5ac77c84c1
Laura Alvarez <apla02@gmail.com>
2024-02-02 09:11:55 -0500

    deleting some comments in app.py file

--- 3_g_challenge/app.py
+++ 3_g_challenge/app.py
@@ -1,60 +1,9 @@
 from app import app
 
-"""
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-db = 'company_db'
-
-@app.route("/")
-def hello():
-    return " hello. World"
-
-def init_database():
-    conn = sqlite3.connect(db)
-    cursor = conn.cursor()
-
-    commands = ('''
-
-        CREATE TABLE IF NOT EXISTS department(
-            id INT PRIMARY KEY,
-            department VARCHAR(26),
-        '''
-        ,
-        '''
-        CREATE TABLE IF NOT EXISTS jobs(
-            id INT PRIMARY KEY,
-            job VARCHAR(26)
-        )
-        '''
-        ,
-        '''
-        CREATE TABLE IF NOT EXISTS employees(
-            id INT PRIMARY KEY,
-            name VARCHAR(26),
-            datetime DATETIME
-            department_id INT,
-            FOREIGN KEY (department_id) REFERENCES departments (id)
-            job_id INT,
-            FOREIGN KEY (job_id) REFERENCES jobs (id)
-        )
-        ''')
-
-    for command in commands:
-         cursor.execute(command)
-    
-    conn.commit()
-    conn.close()
-
-
-
-"""
 """from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
-from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
